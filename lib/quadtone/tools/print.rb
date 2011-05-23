7abba8c8e05eaea604fac60083f7b95ef92d7728
John Labovitz <johnl@johnlabovitz.com>
2011-05-22 18:25:08 -0700

    Remove '--media' option in print tool (use --option/s instead). Add '--option' (aka --options and -o) for specifying printer options for job. Ensure that QTRIP16 mode (16-bit printing mode) is always set (unless in calibration mode). Add '--show-options' to print tool.

--- lib/quadtone/tools/print.rb
+++ lib/quadtone/tools/print.rb
@@ -6,22 +6,33 @@ module Quadtone
   class PrintTool < Tool
   
     attr_accessor :calibrate
-    attr_accessor :media
+    attr_accessor :options
+    
+    def initialize
+      super
+      @options = {}
+    end
     
     def parse_option(option, args)
       case option
       when '--calibrate', '-c'
         @calibrate = true
-      when '--media', '-m'
-        @media = args.shift
+      when '--show-options'
+        @show_options = true
+      when '--option', '--options', '-o'
+        @options.merge!(
+          Hash[
+            args.shift.split(',').map { |o| o.split('=') }
+          ]
+        )
       end
     end
     
     def run(*image_files)
       profile = Profile.from_dir(@profile_dir)
-      options = {}
-      options['ColorModel'] = 'QTCAL' if @calibrate
-      options['Media'] = @media if @media
+      options = @options.dup
+      options['ColorModel'] = @calibrate ? 'QTCAL' : 'QTRIP16'
+      profile.dump_printer_options if @show_options
       image_files.each { |p| Pathname.new(p) }.each do |image_path|
         profile.print_image(image_path, options)
       end
