e333513c15713d2f0fc3233ba7628484f680e036
John Labovitz <johnl@johnlabovitz.com>
2012-03-24 10:24:08 -0700

    Simplify #with_extname logic.

--- lib/quadtone/extensions/pathname3.rb
+++ lib/quadtone/extensions/pathname3.rb
@@ -1,7 +1,7 @@
 class Pathname
   
   def with_extname(extname)
-    Pathname.new(to_s.sub(/\.[^.]+$/, extname))
+    Pathname.new(to_s.sub(/#{Regexp.quote(self.extname)}$/, extname))
   end
   
 end
