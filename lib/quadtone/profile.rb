3b42d02ac52dc0444382133f55349104d5aa3899
John Labovitz <johnl@johnlabovitz.com>
2014-03-01 17:55:47 -0500

    Call CUPS API properly. Whitespace.

--- lib/quadtone/profile.rb
+++ lib/quadtone/profile.rb
@@ -1,7 +1,7 @@
 module Quadtone
-  
+
   class Profile
-    
+
     attr_accessor :name
     attr_accessor :printer
     attr_accessor :printer_options
@@ -15,12 +15,12 @@ module Quadtone
     attr_accessor :gray_overlap
     attr_accessor :gray_gamma
     attr_accessor :mtime
-    
+
     ProfileName = 'profile'
     CharacterizationName = 'characterization'
     LinearizationName = 'linearization'
     ImportantPrinterOptions = %w{MediaType Resolution ripSpeed stpDither}
-    
+
     def self.from_dir(dir='.')
       file = Pathname.new(dir) + "#{ProfileName}.yaml"
       profile = YAML::load(file.open.read)
@@ -28,7 +28,7 @@ module Quadtone
       profile.setup
       profile
     end
-    
+
     def initialize(params={})
       @mtime = Time.now
       @printer_options = {}
@@ -40,30 +40,30 @@ module Quadtone
       params.each { |key, value| method("#{key}=").call(value) }
       setup
     end
-    
+
     def profile_path
       Pathname.new("#{ProfileName}.yaml")
     end
-    
+
     def characterization_ti3_path
       Pathname.new("#{CharacterizationName}.ti3")
     end
-    
+
     def linearization_ti3_path
       Pathname.new("#{LinearizationName}.ti3")
     end
-    
+
     def qtr_profile_path
       Pathname.new(@name + '.txt')
     end
-    
+
     def quad_file_path
       Pathname.new('/Library/Printers/QTR/quadtone') + @printer + "#{@name}.quad"
     end
-    
+
     def setup
       raise "No printer specified" unless @printer
-      @ppd = CupsPPD.new(@printer.dup)
+      @ppd = CupsPPD.new(@printer.dup, nil)
       ppd_options = @ppd.options
       ImportantPrinterOptions.each do |option_name|
         option = ppd_options.find { |o| o[:keyword] == option_name }
@@ -82,11 +82,11 @@ module Quadtone
       read_characterization_curveset!
       read_linearization_curveset!
     end
-    
+
     def to_yaml_properties
       super - [:@characterization_curveset, :@linearization_curveset, :@ppd]
     end
-        
+
     def read_characterization_curveset!
       if characterization_ti3_path.exist?
         @characterization_curveset = CurveSet.from_ti3_file(characterization_ti3_path, Color::QTR)
@@ -94,7 +94,7 @@ module Quadtone
         warn "No characterization file: #{characterization_ti3_path}"
       end
     end
-    
+
     def read_linearization_curveset!
       if linearization_ti3_path.exist?
         if characterization_ti3_path.exist? && linearization_ti3_path.mtime > characterization_ti3_path.mtime && linearization_ti3_path.mtime > @mtime
@@ -106,62 +106,62 @@ module Quadtone
         warn "No linearization file: #{linearization_ti3_path}"
       end
     end
-    
+
     def save!
       profile_path.open('w') { |fh| YAML::dump(self, fh) }
     end
-    
+
     def initial_characterization_curveset
       CurveSet.new(:color_class => Color::QTR, :channels => @inks, :limits => @limits)
     end
-    
+
     def initial_linearization_curveset
       CurveSet.new(:color_class => Color::Gray)
     end
-    
+
     def build_targets
       initial_characterization_curveset.build_target(CharacterizationName)
       initial_linearization_curveset.build_target(LinearizationName)
     end
-    
+
     def measure_targets(options)
       initial_characterization_curveset.measure_target(CharacterizationName) if options[:characterization]
       initial_linearization_curveset.measure_target(LinearizationName) if options[:linearization]
     end
-    
+
     def qtr_profile(io)
-      
+
       raise "No characterization is set" unless @characterization_curveset
-      
+
       io.puts "PRINTER=#{@printer}"
       io.puts "GRAPH_CURVE=NO"
       io.puts
-      
+
       io.puts "N_OF_INKS=#{@characterization_curveset.num_channels}"
       io.puts
-      
+
       io.puts "DEFAULT_INK_LIMIT=#{@default_limit * 100}"
       @characterization_curveset.curves.each do |curve|
         io.puts "LIMIT_#{curve.key}=#{curve.limit.input * 100}"
       end
       io.puts
-      
+
       io.puts "N_OF_GRAY_PARTS=#{@characterization_curveset.num_channels}"
       io.puts
-      
+
       @characterization_curveset.separations.each_with_index do |separation, i|
         channel, input = *separation
         io.puts "GRAY_INK_#{i+1}=#{channel}"
         io.puts "GRAY_VAL_#{i+1}=#{input * 100}"
         io.puts
       end
-      
+
       io.puts "GRAY_HIGHLIGHT=#{@gray_highlight}"
       io.puts "GRAY_SHADOW=#{@gray_shadow}"
       io.puts "GRAY_OVERLAP=#{@gray_overlap}"
       io.puts "GRAY_GAMMA=#{@gray_gamma}"
       io.puts
-      
+
       if @linearization_curveset
         curve = @linearization_curveset.curves.first
         samples = curve.interpolated_samples(21)
@@ -171,16 +171,16 @@ module Quadtone
         io.puts "LINEARIZE=\"#{samples.map { |s| s.output * 100 }.join(' ')}\""
       end
     end
-    
+
     def write_qtr_profile
       ;;warn "writing QTR profile to #{qtr_profile_path}"
       qtr_profile_path.open('w') { |io| qtr_profile(io) }
     end
-  
+
     def install
       system('/Library/Printers/QTR/bin/quadprofile', qtr_profile_path)
     end
-    
+
     def print_image(image_path, options={})
       printer = CupsPrinter.new(@printer.dup)
       options['ripCurve1'] = @name if options['ColorModel'] != 'QTCAL'
@@ -193,7 +193,7 @@ module Quadtone
       end
       printer.print_file(image_path, options)
     end
-    
+
     def page_size(name=nil)
       name ||= @ppd.attribute('DefaultPageSize').first[:value]
       size = @ppd.page_size(name)
@@ -201,7 +201,7 @@ module Quadtone
       size[:imageable_height] = (size[:margin][:top] - size[:margin][:bottom]).pt
       size
     end
-    
+
     def print_printer_attributes
       puts "Attributes:"
       @ppd.attributes.sort_by { |a| a[:name] }.each do |attribute|
@@ -223,7 +223,7 @@ module Quadtone
         ]
       end
     end
-    
+
     def to_html
       html = Builder::XmlMarkup.new(:indent => 2)
       html.declare!(:DOCTYPE, :html)
@@ -266,5 +266,5 @@ module Quadtone
     end
 
   end
-  
+
 end
