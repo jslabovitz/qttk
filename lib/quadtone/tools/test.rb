44f2993af1895dd36518f9df61c3e65086b03dc6
John Labovitz <johnl@johnlabovitz.com>
2014-03-01 23:13:29 -0500

    Whitespace.

--- lib/quadtone/tools/test.rb
+++ lib/quadtone/tools/test.rb
@@ -2,37 +2,37 @@ require 'quadtone'
 include Quadtone
 
 module Quadtone
-  
+
   class TestTool < Tool
-    
+
     def run
       # - test linearization
-      # 
+      #
       #   - print grayscale target image with QTR curve, then measure target
       #   - analyze measured target
       #     - build grayscale curve from samples
       #     - test for linear response
       #     - show dMin/dMax, Lab curve
-      # 
+      #
       #   - store each test with timestamp
-      # 
+      #
       #   - chart scale over time (with multiple tests)
       #     - graph differences between values
       #     - graph average dE
       #       - see: http://cias.rit.edu/~gravure/tt/pdf/pc/TT5_Fred01.pdf (p. 34)
 
       # test_grayscale_name = 'test-grayscale'
-      # 
+      #
       # test_grayscale_measured_path = Pathname.new(test_grayscale_name + '.measured.txt')
-      # 
+      #
       # wait_for_file(test_grayscale_measured_path, "print & measure target #{grayscale_reference_path} -- save data to #{test_grayscale_measured_path}")
-      # 
+      #
       # test_grayscale_measured_target = Target.from_ti3_file(test_grayscale_measured_path)
       # test_grayscale_measured_curveset = CurveSet.from_samples(test_grayscale_measured_target.samples)
       # test_grayscale_measured_curveset.write_svg_file(test_grayscale_measured_path.with_extname('.svg'))
 
       #FIXME: See above
-      
+
       image_file = Pathname.new('test.tif')
       image_list = Magick::ImageList.new
       begin
@@ -61,9 +61,9 @@ module Quadtone
       final_image = image_list.append(true)
       ;;warn "writing #{image_file}"
       final_image.write(image_file) { self.compression = Magick::ZipCompression }
-      
+
     end
-  
+
   end
-  
+
 end
