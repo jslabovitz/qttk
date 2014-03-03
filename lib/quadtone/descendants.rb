d0d5d29e2e64d050c6d3a5a1fff9e9ea376cd01e
John Labovitz <johnl@johnlabovitz.com>
2014-03-02 19:25:48 -0500

    Syntax tweak.

--- lib/quadtone/descendants.rb
+++ lib/quadtone/descendants.rb
@@ -1,9 +1,9 @@
 # from http://stackoverflow.com/a/2393878
 
 class Class
-  
+
   def descendants
-    ObjectSpace.each_object(::Class).select {|klass| klass < self }
+    ObjectSpace.each_object(::Class).select { |c| c < self }
   end
-  
+
 end
