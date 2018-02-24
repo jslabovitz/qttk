9696158d5a41f4d013b5ed1a2fbcaa483820ae68
John Labovitz <johnl@johnlabovitz.com>
2018-02-24 17:57:49 -0600

    Refactor 'edit' tool.

--- lib/quadtone/tools/edit.rb
+++ lib/quadtone/tools/edit.rb
@@ -5,12 +5,11 @@ module Quadtone
     class Edit < Tool
 
       def run
-        # editor = ENV['EDITOR'] or raise "Can't determine editor"
-        editor = 'subl'
-        Quadtone.run(editor,
+        editor = ENV['EDITOR'] or raise "Can't determine editor"
+        system(editor,
           '--wait',
-          @profile.qtr_profile_path)
-        @profile = Profile.load(@profile.qtr_profile_path)
+          @profile.qtr_profile_path.to_s)
+        @profile = Profile.load(@profile.name)
         @profile.check
         @profile.save
         @profile.show
